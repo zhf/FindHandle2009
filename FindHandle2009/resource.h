718af145d19c51e98b2a6d26b793484f92f123a1
zhf <hf@zhanghongfeng.com>
2012-12-29 01:05:25 +0800

    Moving histrical project to Github

---
+++ FindHandle2009/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FindHandle2009.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FINDHANDLE2009_DIALOG       102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_FINDHANDLE2009              107
+#define IDI_SMALL                       108
+#define IDC_FINDHANDLE2009              109
+#define IDR_MAINFRAME                   128
+#define ID_TRACE_START                  32773
+#define ID_TRACE_STOP                   32774
+#define IDM_TRACE_START                 32775
+#define IDM_TRACE_STOP                  32776
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
